2b595fc773f6819d79ae3fc02dd2d30c8cb20d9f
Kelvin <kelvinmusipha@gmail.com>
2024-04-24 06:10:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
